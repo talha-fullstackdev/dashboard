e3e8fb23642535e1f7edd69594cf0c3c67b34be3
Talha-TN <nawazt964@gmail.com>
2025-01-30 13:05:03 -0800

    connected data base

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,21 @@
-import express from "express"
+import express from "express";
 import dotenv from "dotenv";
-const app = express()
-app.get("/",(req,res)=>{
-    res.send("server has started ")
-})
-const PORT =  process.env.PORT || 8080;
-app.listen(PORT,()=>{
-    console.log(`server has started at http://localhost:${PORT}`)
-})
+import connectDB from "./models/db.js";
+// Load environment variables
+dotenv.config();
+connectDB();
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Server has started");
+});
+app.get("/user", (req, res) => {
+    res.send("User is Talha Nawaz and admin is also Talha Nawaz");
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server has started at http://localhost:${PORT}`);
+});
---
+++ backend/models/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// Load environment variables
+dotenv.config();
+const mongo_url = process.env.MONGO_CONNECTION;
+const connectDB = async () => {
+    try {
+        await mongoose.connect(mongo_url);
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.error("Database connection failed", error);
+        process.exit(1); // Exit process if DB connection fails
+    }
+};
+
+export default connectDB;

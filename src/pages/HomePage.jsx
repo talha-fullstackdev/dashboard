2e968f28c4e95239487f8df7a1b73668a89c31d5
Talha-TN <nawazt964@gmail.com>
2025-02-03 14:43:04 -0800

    displayed the name of login user in home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import useTitle from '../hooks/UseTitle'
-import { Link } from 'react-router'
+import React, { useEffect, useState } from "react";
+import useTitle from "../hooks/UseTitle";
+import { Link } from "react-router";
 const HomePage = () => {
-  useTitle("Home Page")
+  useTitle("Home Page");
+  const [loginUser, setLoginUser] = useState("");
+  useEffect(() => {
+    setLoginUser(localStorage.getItem("loggedInUser"));
+  });
   return (
-    <div >
-      <h1 className='text-4xl mb-4 font-bold'>Welcome to home page</h1>
-      <Link className='ml-4 rounded-lg bg-gradient-to-r from-blue-500 to-purple-500 hover:from-purple-500 hover:to-blue-500 text-white px-2 py-1 ' to="/login">logout</Link>
+    <div>
+      <h1 className="text-4xl mb-4 font-bold">
+        Welcome to home page {loginUser}
+      </h1>
+      <Link
+        className="ml-4 rounded-lg bg-gradient-to-r from-blue-500 to-purple-500 hover:from-purple-500 hover:to-blue-500 text-white px-2 py-1 "
+        to="/login"
+      >
+        logout
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;

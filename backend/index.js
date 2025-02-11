df66d0d3b54fa6a8af3e7bae963f02fbacac30d3
talha-fullstackdev <nawazt964@gmail.com>
2025-02-11 10:40:10 -0800

    remove double title tag

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ connectDB();
 const app = express();
 app.use(express.json());
 app.use(bodyParser.json()) //parse the json data into javascript object
-app.use(cors()
+app.use(cors())
 app.use("/auth",router)
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
